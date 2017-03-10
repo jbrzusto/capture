86ef985fe7bf07bd0d9a8bbea1f59ac48afa2ccc
john brzustowski <jbrzusto@fastmail.fm>
2017-03-10 18:51:55 +0000

    update script to use longer-range parameters

--- scripts/runCapture.sh
+++ scripts/runCapture.sh
@@ -3,4 +3,4 @@
 cd ~/capture
 date >> capturelog.txt
 export DATADIR=/radar_spool
-scripts/capture -d 2 -n 2496 -p 7500 -c 1 -r 0.43:0.12 >> capturelog.txt 2>&1
+scripts/capture -d 2 -n 3744 -p 7500 -c 1 -r 0.43:0.12 >> capturelog.txt 2>&1
